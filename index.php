a771a06a654c9e110794b9674a47648d3f1c292e
Anthony Barretti <anthonybarretti@Anthonys-MBP.fritz.box>
2017-11-18 08:53:55 +0100

    altered views and using SILEX solely as controller

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
 
 date_default_timezone_set("America/New_York");
@@ -10,28 +8,28 @@ $app = new Silex\Application();
 
 $app['debug']=true;
 
-use ABS\Classes\ModelClass as ModelClass;
-use ABS\Classes\ControllerClass as ControllerClass;
-
-$model = new ModelClass();
-$controller= new ControllerClass($model);
-
-if (isset($_GET['action']))
-{
-	$controller->{$_GET['action']}();
-}
+$url= 'http://'.$_SERVER['SERVER_NAME'];
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array( 
 		'twig.path' => __DIR__.'/views',
 	));
 
-$app->get('/', function () use ($app, $model) {
-	return $app['twig']->render($model->page, []);
+$app->get('/', function () use ($app, $url) {
+	return $app['twig']->render('home.twig', ['url'=>$url]);
+});
+
+$app->get('/about', function () use ($app, $url) {
+	return $app['twig']->render('about.twig', ['url'=>$url]);
+});
+
+$app->get('/cv', function () use ($app, $url) {
+	return $app['twig']->render('cv.twig', ['url'=>$url]);
+});
+
+$app->get('/contact', function () use ($app, $url) {
+	return $app['twig']->render('contact.twig', ['url'=>$url]);
 });
 
 $app->run();
 
-?>
-</html>
-
-<!-- php Desktop/PHP/AnthonyBarrettiSiteSILEX/index.php -->
+// php Desktop/PHP/AnthonyBarrettiSiteSILEX/index.php -->
